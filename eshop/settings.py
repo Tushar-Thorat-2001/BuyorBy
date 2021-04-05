4b134b153670c8610ab52af15b5c1d5763a77ed6
Tushar <thorat20187@gmail.com>
2021-04-05 20:48:25 +0530

    second commit

--- eshop/settings.py
+++ eshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*a5!jgvxy466--atslf0e2xki6d&w1p_1r0@4s%&$opg5yme^z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
